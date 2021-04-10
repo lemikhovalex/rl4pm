9ca6deedda88d5fd90b2a7d92e97cef1fbab2294
Lemikhov Alex <lemikhovalex@gmail.com>
2021-04-10 12:38:43 +0300

    dunno why it doesnt require grad

---
+++ rl4pm_lib/agencies.py
@@ -0,0 +1,87 @@
+import torch
+from .agents import AgentAct, AgentTeDiscrete
+from .utils import play_and_record
+from torch.nn import functional as t_functional
+
+
+class Agency:
+    def __init__(self, input_size, hidden, n_lstm, te_intervals, ac_learning_rate,
+                 te_learning_rate, n_classes, discount_factor):
+        self.te_agent = AgentTeDiscrete(input_size=input_size, hidden_layer=hidden, n_lstm=n_lstm,
+                                        te_intervals=te_intervals)
+        self.ac_agent = AgentAct(input_size=input_size, hidden_layer=hidden, n_lstm=n_lstm,
+                                 out_shape=n_classes)
+        self.discount_factor = discount_factor
+        self.te_agent_targ = AgentTeDiscrete(input_size=input_size, hidden_layer=hidden, n_lstm=n_lstm,
+                                             te_intervals=te_intervals)
+        self.ac_agent_targ = AgentAct(input_size=input_size, hidden_layer=hidden, n_lstm=n_lstm,
+                                      out_shape=n_classes)
+        self.refresh_target()
+
+        self.te_opt = torch.optim.Adam(self.te_agent.parameters(), lr=ac_learning_rate)
+        self.ac_opt = torch.optim.Adam(self.ac_agent.parameters(), lr=te_learning_rate)
+
+    def refresh_target(self, polyak_avg=1):
+        for target_param, param in zip(self.te_agent_targ.parameters(), self.te_agent.parameters()):
+            target_param.data.copy_(polyak_avg * param + (1 - polyak_avg) * target_param)
+
+        for target_param, param in zip(self.ac_agent_targ.parameters(), self.ac_agent.parameters()):
+            target_param.data.copy_(polyak_avg * param + (1 - polyak_avg) * target_param)
+
+    def train(self, env, exp_replay, batch_size):
+
+        # play and record
+        _ = env.reset()
+        with torch.no_grad():
+            episode_reward, _ = play_and_record(self.te_agent, self.ac_agent, env, exp_replay)
+
+        # get data from memory
+
+        # train
+        upd_size = batch_size if len(exp_replay) > batch_size else len(exp_replay) // 2
+        states, actions_te, actions_ac, rewards_te, rewards_ac, next_states, is_dones = exp_replay.sample(upd_size)
+
+        te_agent_loss, ac_agent_loss = self.get_losses(states, actions_te, actions_ac, rewards_te, rewards_ac,
+                                                       next_states, is_dones)
+
+        # update networks
+        self.te_opt.zero_grad()
+        te_agent_loss.backward()
+        self.te_opt.step()
+
+        self.ac_opt.zero_grad()
+        ac_agent_loss.backward()
+        self.ac_opt.step()
+
+        return episode_reward
+
+    def get_loss_discrete_agent(self, states, actions, rewards,
+                                next_states, is_dones, agent):
+        is_dones = is_dones.view(-1)
+
+        qs, _ = agent(states)
+        print(f'Agency.get_loss_discrete_agent:: qs.requires_grad={qs.requires_grad}')
+        predicted_q_values = torch.gather(input=qs, index=actions, dim=2)
+
+        with torch.no_grad():
+            qs_next, _ = agent(next_states)
+            actions_idx_next = agent.sample_action_from_q(qs_next, stoch=True)
+        predicted_next_q_values = torch.gather(input=qs_next, index=actions_idx_next.unsqueeze(2), dim=2)
+
+        q_reference = rewards + self.discount_factor * predicted_next_q_values
+        q_reference = q_reference.view(-1, 1)[is_dones.logical_not()]
+        predicted_q_values = predicted_q_values.view(-1, 1)[is_dones.logical_not()]
+
+        # # mean squared error loss to minimize
+        loss = t_functional.smooth_l1_loss(predicted_q_values, q_reference).mean()
+
+        return loss
+
+    def get_losses(self, states, actions_te, actions_ac, rewards_te, rewards_ac,
+                   next_states, is_dones):
+        te_agent_loss = self.get_loss_discrete_agent(states, actions_te, rewards_te,
+                                                     next_states, is_dones, agent=self.te_agent)
+
+        ac_agent_loss = self.get_loss_discrete_agent(states, actions_ac, rewards_ac,
+                                                     next_states, is_dones, agent=self.ac_agent)
+        return te_agent_loss, ac_agent_loss
---
+++ rl4pm_lib/agents.py
@@ -0,0 +1,80 @@
+import torch
+
+class NetAgent(torch.nn.Module):
+    def __init__(self, input_size, hidden_layer, n_lstm, out_shape):
+        super(NetAgent, self).__init__()
+        self.lstm = torch.nn.LSTM(input_size=input_size, hidden_size=hidden_layer, batch_first=True, num_layers=n_lstm)
+        self.relu = torch.nn.ReLU()
+        self.fc = torch.nn.Linear(hidden_layer, out_shape)
+
+    def forward(self, x, h):
+        x, (h, c) = self.lstm(x, (h[0], h[1]))
+        x = self.relu(x)
+        x = self.fc(x)
+        return x, (h, c)
+
+
+class AgentAct(torch.nn.Module):
+    def __init__(self, input_size, hidden_layer, n_lstm, out_shape):
+        super(AgentAct, self).__init__()
+        self.net = NetAgent(input_size, hidden_layer, n_lstm, out_shape)
+        self.target_net = NetAgent(input_size, hidden_layer, n_lstm, out_shape)
+        self.hidden = hidden_layer
+
+    def forward(self, x, hidden=None):
+        if hidden is None:
+            h_te = torch.zeros((1, x.shape[0], self.hidden), requires_grad=True)
+            c_te = torch.zeros((1, x.shape[0], self.hidden), requires_grad=True)
+            hidden = (h_te, c_te)
+        return self.net(x, hidden)
+
+    def sample_action(self, x, hidden, stoch=False):
+        q_values, hidden = self.net(x, hidden)
+
+        act_idx = self.sample_action_from_q(q_values, stoch=stoch)
+        act_idx = act_idx.view(act_idx.shape[0])
+        return act_idx, hidden
+
+    def sample_action_from_q(self, q_values, stoch=False):
+        if not stoch:
+            act_idx = q_values.argmax(dim=2)
+        else:
+            act_idx = q_values.argmax(dim=2)
+        return act_idx
+
+
+class AgentTeDiscrete(torch.nn.Module):
+    def __init__(self, input_size, hidden_layer, n_lstm, te_intervals):
+        super(AgentTeDiscrete, self).__init__()
+        self.net = NetAgent(input_size, hidden_layer, n_lstm, len(te_intervals))
+        self.target_net = NetAgent(input_size, hidden_layer, n_lstm, len(te_intervals))
+        self.te_intervals = te_intervals
+        self.hidden = hidden_layer
+
+    def forward(self, x, hidden=None):
+        if hidden is None:
+            h_te = torch.zeros((1, x.shape[0], self.hidden), requires_grad=True)
+            c_te = torch.zeros((1, x.shape[0], self.hidden), requires_grad=True)
+            hidden = (h_te, c_te)
+        return self.net(x, hidden)
+
+    def sample_action(self, x, hidden, stoch=False):
+        q_values, hidden = self.net(x, hidden)
+
+        t_idx = self.sample_action_from_q(q_values, stoch)
+        t_idx = t_idx.view(t_idx.shape[0])
+        return t_idx, hidden
+
+    def sample_action_from_q(self, q_values, stoch=False):
+        # print(f'AgentTeDiscrete.sample_action_from_q:: q_values.shape={q_values.shape}')
+        if not stoch:
+            t_idx = q_values.argmax(dim=2)
+        else:
+            t_idx = q_values.argmax(dim=2)
+        return t_idx
+
+    def act_to_te(self, t_idx):
+        out = torch.zeros(t_idx.shape)
+        for i in range(out.shape[0]):
+            out[i] = (self.te_intervals[t_idx[i]][0] + self.te_intervals[t_idx[i]][1]) / 2.
+        return out
