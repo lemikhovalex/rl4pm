dc1d0ba3b3d32c91f8b796fee144f0748bc47bb9
Aleksandr <lemikhovalex@gmail.com>
2021-04-18 16:16:46 +0300

    scaling added

--- rl4pm_lib/envs.py
+++ rl4pm_lib/envs.py
@@ -12,8 +12,7 @@ def get_next_input(prev_inp, next_act, next_te, column_feature, device):
 
     next_event[:, column_feature['tw']] = (last_event[:, column_feature['tw']] + next_te) % (7 * 24 * 60 * 60)
     # one hot transformation from https://discuss.pytorch.org/t/convert-int-into-one-hot-format/507/5
-    act_onehot = torch.FloatTensor(out.shape[0], out.shape[2] - len(column_feature), device=device)
-    act_onehot.zero_()
+    act_onehot = torch.zeros(out.shape[0], out.shape[2] - len(column_feature), device=device)
     act_onehot.scatter_(1, next_act.long().view(-1, 1), 1)
     next_event[:, len(column_feature):] = act_onehot
 
@@ -24,25 +23,30 @@ def get_next_input(prev_inp, next_act, next_te, column_feature, device):
 def get_te_reward(true: torch.tensor, pred: torch.tensor, intervals):
     masks = []
     for inter in intervals:
-        true_here = (true >= inter[0]) * (true < inter[1])
-        pred_here = (pred >= inter[0]) * (pred < inter[1])
+        true_here = (true > inter[0]) * (true <= inter[1])
+        pred_here = (pred > inter[0]) * (pred <= inter[1])
+        assert true_here.shape == pred_here.shape
         masks.append(true_here * pred_here)
     out = torch.stack(masks).T.sum(dim=1)
     return out
 
 
 def get_act_reward(true_act_oh, pred_act_oh):
+    assert true_act_oh.shape == pred_act_oh.shape
     mult = (true_act_oh * pred_act_oh)
     return mult.sum(dim=1)
 
 
 class PMEnv(gym.Env):
-    def __init__(self, data: torch.tensor, intervals_te_rew, column_to_time_features, window_size, device=None):
+    def __init__(self, data: torch.tensor, intervals_te_rew, column_to_time_features, window_size,
+                 device=None, scales=None):
         if device is None:
             device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        if scales is None:
+            scales = {'te': 1., 'tw': 1., 'tt': 1.}
+        self.scales = scales
         self.device = device
-        self.data = data
-        self.data.to(self.device)
+        self.data = data.clone().detach().to(self.device)
         self.pred_counter = window_size
         self.trace_index = None
         self.intervals = intervals_te_rew
@@ -62,6 +66,10 @@ class PMEnv(gym.Env):
         returns: next_s, (reward_te, reward_act), is_done, add_inf
         """
         true_te = self.data[:, self.pred_counter, self.column_feature['te']]
+        # print('envs.PMEnv.step::')
+        # print(f'\tself.data.device={self.data.device}')
+        # print(f'\ttrue_te.device={true_te.device}')
+        # print(f'\tnext_te.device={next_te.device}')
         te_rew = get_te_reward(true=true_te, pred=next_te, intervals=self.intervals)
         # (f'te_reward = \n{te_rew}')
         true_act_oh = self.data[:, self.pred_counter, len(self.column_feature):]
@@ -71,15 +79,10 @@ class PMEnv(gym.Env):
         pred_act_oh[range(pred_act_oh.shape[0]), next_act.long()] = 1
 
         act_rew = get_act_reward(true_act_oh=true_act_oh, pred_act_oh=pred_act_oh)
-        print('envs.PMEnv.step::')
-        print(f'\tself.given_state.device={self.given_state.device}')
-        print(f'\tnext_act.device={next_act.device}')
-        print(f'\tnext_te.device={next_te.device}')
-        print(f'\tself.device={self.device}')
         next_s = get_next_input(prev_inp=self.given_state,
                                 next_act=next_act, next_te=next_te,
                                 column_feature=self.column_feature, device=self.device)
-        self.given_state = next_s
+        self.given_state = next_s.clone()
 
         is_done = next_s[:, self.win - 1, len(self.column_feature):].sum(axis=1)
         is_done *= -1
@@ -88,6 +91,10 @@ class PMEnv(gym.Env):
         is_done = is_done.bool()
 
         self.pred_counter += 1
+        # do scaling
+        for _tf in self.scales:
+            next_s[:, :, self.column_feature[_tf]] = next_s[:, :, self.column_feature[_tf]] / self.scales[_tf]
+
         return next_s, (te_rew, act_rew), is_done, {}
 
     def to(self, device):
