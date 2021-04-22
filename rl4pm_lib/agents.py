3989047027a47d1f01e1e45f2e72bb9b92bbff24
Aleksandr <lemikhovalex@gmail.com>
2021-04-22 11:00:32 +0300

    removed duplicates

--- rl4pm_lib/agents.py
+++ rl4pm_lib/agents.py
@@ -2,6 +2,19 @@ import torch
 from torch.nn import functional as t_functional
 
 
+def sample_action_from_q(q_values, stoch=False):
+    if stoch:
+        _max = torch.max(q_values, dim=2, keepdim=True).values
+        _min = torch.min(q_values, dim=2, keepdim=True).values
+        _sc = _max - _min
+        q_values = q_values / _sc
+        dist = torch.distributions.Categorical(t_functional.softmax(q_values, dim=1))
+        t_idx = dist.sample()
+    else:
+        t_idx = q_values.argmax(dim=2)
+    return t_idx
+
+
 class NetAgent(torch.nn.Module):
     def __init__(self, input_size, hidden_layer, n_lstm, out_shape):
         super(NetAgent, self).__init__()
@@ -16,12 +29,11 @@ class NetAgent(torch.nn.Module):
         return x, h
 
 
-class AgentAct(torch.nn.Module):
+class BaseAgent(torch.nn.Module):
     def __init__(self, input_size, hidden_layer, n_lstm, out_shape):
-        super(AgentAct, self).__init__()
+        super(BaseAgent, self).__init__()
         self.n_lstm = n_lstm
         self.net = NetAgent(input_size, hidden_layer, n_lstm, out_shape)
-        self.target_net = NetAgent(input_size, hidden_layer, n_lstm, out_shape)
         self.hidden = hidden_layer
 
     def forward(self, x, hidden=None):
@@ -39,56 +51,26 @@ class AgentAct(torch.nn.Module):
         return act_idx, hidden
 
     def sample_action_from_q(self, q_values, stoch=False):
-
-        if stoch:
-            _max = torch.max(q_values, dim=2, keepdim=True).values
-            _min = torch.min(q_values, dim=2, keepdim=True).values
-            _sc = _max - _min
-            q_values = q_values / _sc
-            dist = torch.distributions.Categorical(t_functional.softmax(q_values, dim=1))
-            act_idx = dist.sample()
-
-        else:
-            act_idx = q_values.argmax(dim=2)
-            # act_idx = q_values.argmax(dim=2)
-        return act_idx
+        return sample_action_from_q(q_values=q_values, stoch=stoch)
 
 
-class AgentTeDiscrete(torch.nn.Module):
-    def __init__(self, input_size, hidden_layer, n_lstm, te_intervals):
-        super(AgentTeDiscrete, self).__init__()
+class AgentAct(BaseAgent):
+    def __init__(self, input_size, hidden_layer, n_lstm, out_shape):
+        super(AgentAct, self).__init__(input_size=input_size, hidden_layer=hidden_layer, n_lstm=n_lstm,
+                                       out_shape=out_shape)
         self.n_lstm = n_lstm
-        self.net = NetAgent(input_size, hidden_layer, n_lstm, len(te_intervals))
-        self.target_net = NetAgent(input_size, hidden_layer, n_lstm, len(te_intervals))
-        self.te_intervals = te_intervals
+        self.net = NetAgent(input_size, hidden_layer, n_lstm, out_shape)
         self.hidden = hidden_layer
 
-    def forward(self, x, hidden=None):
-        if hidden is None:
-            h_te = torch.zeros((self.n_lstm, x.shape[0], self.hidden), requires_grad=True)
-            c_te = torch.zeros((self.n_lstm, x.shape[0], self.hidden), requires_grad=True)
-            hidden = (h_te, c_te)
-        return self.net(x, hidden)
 
-    def sample_action(self, x, hidden=None, stoch=False):
-        q_values, hidden = self.net(x, hidden)
-
-        t_idx = self.sample_action_from_q(q_values, stoch)
-        t_idx = t_idx.view(t_idx.shape[0])
-        return t_idx, hidden
-
-    def sample_action_from_q(self, q_values, stoch=False):
-        # print(f'AgentTeDiscrete.sample_action_from_q:: q_values.shape={q_values.shape}')
-        if stoch:
-            _max = torch.max(q_values, dim=2, keepdim=True).values
-            _min = torch.min(q_values, dim=2, keepdim=True).values
-            _sc = _max - _min
-            q_values = q_values / _sc
-            dist = torch.distributions.Categorical(t_functional.softmax(q_values, dim=1))
-            t_idx = dist.sample()
-        else:
-            t_idx = q_values.argmax(dim=2)
-        return t_idx
+class AgentTeDiscrete(BaseAgent):
+    def __init__(self, input_size, hidden_layer, n_lstm, te_intervals):
+        super(AgentTeDiscrete, self).__init__(input_size=input_size, hidden_layer=hidden_layer, n_lstm=n_lstm,
+                                              out_shape=len(te_intervals))
+        self.n_lstm = n_lstm
+        self.net = NetAgent(input_size, hidden_layer, n_lstm, len(te_intervals))
+        self.te_intervals = te_intervals
+        self.hidden = hidden_layer
 
     def act_to_te(self, t_idx):
         device = next(self.parameters()).device
