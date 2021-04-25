c40707c2df8d3037b7e246e2dda003c8d9eaef06
Aleksandr <lemikhovalex@gmail.com>
2021-04-25 17:05:46 +0300

    hope env is well rewriten to numpy

--- rl4pm_lib/envs.py
+++ rl4pm_lib/envs.py
@@ -1,10 +1,10 @@
-import torch
+import numpy as np
 import gym
 from .preprocessing import PaperScaler
 
 
-def get_next_input_replaced_by_pred(prev_inp: torch.tensor, next_act: torch.tensor, next_te: torch.tensor,
-                                    column_feature: dict, window_size: int, device: torch.device):
+def get_next_input_replaced_by_pred(prev_inp: np.ndarray, next_act: np.ndarray, next_te: np.ndarray,
+                                    column_feature: dict, window_size: int):
     """
     construct a new input, based on previous one. s_t = [(a_{t-1}, a_{t}), (te_{t-1}, te_{t}), ...]
     got predicted a'_{t+1}.
@@ -16,28 +16,26 @@ def get_next_input_replaced_by_pred(prev_inp: torch.tensor, next_act: torch.tens
         next_te: predicted next te, shape=[n_traces]
         column_feature: map from not-one-hot features to idex
         window_size: size of window, sent to NN
-        device: divice, where next event will be stored
     Returns: torch tensor, whith state, extended by next predictions, and shorted,
 
     """
     out = prev_inp[:, window_size - 1:]
-    next_event = torch.zeros(prev_inp.shape[0], prev_inp.shape[2], device=device)
+    next_event = np.zeros((prev_inp.shape[0], prev_inp.shape[2]))
     next_event[:, column_feature['te']] = next_te
-    last_event = prev_inp[:, -1].squeeze(1)
+    last_event = np.squeeze(prev_inp[:, -1], axis=1)
 
     next_event[:, column_feature['tt']] = last_event[:, column_feature['tt']] + next_te
 
     next_event[:, column_feature['tw']] = (last_event[:, column_feature['tw']] + next_te) % (7 * 24 * 60 * 60)
     # one hot transformation from https://discuss.pytorch.org/t/convert-int-into-one-hot-format/507/5
-    act_onehot = torch.zeros(out.shape[0], out.shape[2] - len(column_feature), device=device)
-    act_onehot.scatter_(1, next_act.long().view(-1, 1), 1)
+    act_onehot = (np.arange(out.shape[2] - len(column_feature)) == next_act[:, None]).astype(np.float32)
     next_event[:, len(column_feature):] = act_onehot
 
-    out = torch.cat([out, next_event.unsqueeze(1)], dim=1)
+    out = np.concatenate([out, np.expand_dims(next_event, axis=1)], axis=1)
     return out
 
 
-def get_te_reward_categorized(true: torch.tensor, pred: torch.tensor, intervals):
+def get_te_reward_categorized(true: np.ndarray, pred: np.ndarray, intervals):
     """
     This function recieves tensors with predicted next-time delta intervals. It naturally recieves float tensors
     the reward depends on time intervals. If predicted and truth are in the same interal, then +1, else +0
@@ -55,11 +53,11 @@ def get_te_reward_categorized(true: torch.tensor, pred: torch.tensor, intervals)
         true_here = (true > inter[0]) * (true <= inter[1])
         pred_here = (pred > inter[0]) * (pred <= inter[1])
         masks.append(true_here * pred_here)
-    out = torch.stack(masks).sum(dim=0)
+    out = np.stack(masks).sum(axis=0)
     return out
 
 
-def get_ctegorial_reward(true_act_oh: torch.tensor, pred_act_oh: torch.tensor):
+def get_ctegorial_reward(true_act_oh: np.ndarray, pred_act_oh: np.ndarray):
     """
     function to recieve reward for categorized features as  +1 if guessed, 0 if not
     args can be swaped
@@ -72,7 +70,7 @@ def get_ctegorial_reward(true_act_oh: torch.tensor, pred_act_oh: torch.tensor):
     """
     assert true_act_oh.shape == pred_act_oh.shape
     mult = (true_act_oh * pred_act_oh)
-    return mult.sum(dim=1)
+    return mult.sum(axis=1)
 
 
 class PMEnv(gym.Env):
@@ -81,8 +79,7 @@ class PMEnv(gym.Env):
     basicly it is a cover for data, which just manipulate it
     Args:
         scaler(.preprocessing.PaperScaler): it is needed to process data before it comes to Model
-        device(torch.device): divice where the data is stored
-        data(torch.tensor): the data tensor which is parsed trace.
+        data(np.ndarray): the data tensor which is parsed trace.
                             data.shape = (n_traces, max_len, n_features_for_event). Last features are one-hot encoded
                             activity. First ones - time related or custom. All of them must present in column_feature
         intervals_te_rew(list[(float, float)]): this env reacts on next event time prediction as for classification
@@ -92,8 +89,7 @@ class PMEnv(gym.Env):
         window_size(int): next state is a window of size window_size.
     Attributes:
         scaler(.preprocessing.PaperScaler): it is needed to process data before it comes to Model
-        device(torch.device): divice where the data is stored
-        data(torch.tensor): the data tensor which is parsed trace.
+        data(np.ndarray): the data tensor which is parsed trace.
                             data.shape = (n_traces, max_len, n_features_for_event). Last features are one-hot encoded
                             activity. First ones - time related or custom. All of them must present in column_feature
         intervals_te_rew(list[(float, float)]): this env reacts on next event time prediction as for classification
@@ -103,19 +99,15 @@ class PMEnv(gym.Env):
         window_size(int): next state is a window of size window_size.
         trace_index(int): index which will be must be predicted
         win(int): window size (from arg)
-        given_state(torhc.tensor): state, which was return from .step(), or .reset() at previous moment.
+        given_state(np.ndarray): state, which was return from .step(), or .reset() at previous moment.
     """
 
     def render(self, mode='human'):
         pass
 
-    def __init__(self, data: torch.tensor, intervals_te_rew, column_to_time_features, window_size,
-                 device=None, scaler=PaperScaler()):
-        if device is None:
-            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    def __init__(self, data: np.ndarray, intervals_te_rew, column_to_time_features, window_size, scaler=PaperScaler()):
         self.scaler = scaler
-        self.device = device
-        self.data = data.clone().detach().to(self.device)
+        self.data = np.copy(data)
         self.pred_counter = window_size
         self.trace_index = None
         self.intervals = intervals_te_rew
@@ -139,12 +131,11 @@ class PMEnv(gym.Env):
         out = self.scaler.transform(out, inplace=True)
         return out
 
-    def get_next_input(self, prev_inp: torch.tensor, next_act: torch.tensor, next_te: torch.tensor):
+    def get_next_input(self, prev_inp: np.ndarray, next_act: np.ndarray, next_te: np.ndarray):
         return get_next_input_replaced_by_pred(prev_inp=prev_inp, next_act=next_act, next_te=next_te,
-                                               column_feature=self.column_feature, window_size=self.win,
-                                               device=self.device)
+                                               column_feature=self.column_feature, window_size=self.win)
 
-    def step(self, action: (torch.tensor, torch.tensor)):
+    def step(self, action: (np.ndarray, np.ndarray)):
         """
         Basic method for interracting with env.
         Returns:
@@ -164,13 +155,13 @@ class PMEnv(gym.Env):
         # (f'te_reward = \n{te_rew}')
         true_act_oh = self.data[:, self.pred_counter, len(self.column_feature):]
 
-        pred_act_oh = torch.zeros(self.data.shape[0], self.data.shape[-1] - len(self.column_feature),
-                                  dtype=torch.uint8, device=self.device)
+        pred_act_oh = np.zeros((self.data.shape[0], self.data.shape[-1] - len(self.column_feature)),
+                               dtype=int)
         pred_act_oh[range(pred_act_oh.shape[0]), next_act.long()] = 1
 
         act_rew = get_ctegorial_reward(true_act_oh=true_act_oh, pred_act_oh=pred_act_oh)
         next_s = self.get_next_input(prev_inp=self.given_state, next_act=next_act, next_te=next_te)
-        self.given_state = next_s.clone()
+        self.given_state = np.copy(next_s)
 
         is_done = next_s[:, self.win - 1, len(self.column_feature):].sum(axis=1)
         is_done *= -1
@@ -184,21 +175,12 @@ class PMEnv(gym.Env):
 
         return next_s, (te_rew, act_rew), is_done, {}
 
-    def to(self, device: torch.device):
-        """
-        Args:
-            device(torch.device): device where data is stored
-
-        Returns: just as it returns torch.tensor.to(device)
-        """
-        self.data.to(device)
-
 
 class PMEnvOneStepCons(PMEnv):
-    def __init__(self, data: torch.tensor, intervals_te_rew: list, column_to_time_features: dict, window_size: int,
-                 device=None, scaler=PaperScaler()):
+    def __init__(self, data: np.ndarray, intervals_te_rew: list, column_to_time_features: dict, window_size: int,
+                 scaler=PaperScaler()):
         super(PMEnvOneStepCons, self).__init__(data, intervals_te_rew, column_to_time_features, window_size,
-                                               device=device, scaler=scaler)
+                                               scaler=scaler)
 
-    def get_next_input(self, prev_inp: torch.tensor, next_act: torch.tensor, next_te: torch.tensor):
+    def get_next_input(self, prev_inp: np.ndarray, next_act: np.ndarray, next_te: np.ndarray):
         return self.data[:, self.pred_counter - self.win:self.pred_counter, :]
